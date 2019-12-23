a63235fbbef536a691f74b21a983ccf117ec39dc
lihongtao13161761865 <13161761865@163.com>
2019-12-23 13:41:16 +0800

    Clean

--- app/fetch/get.js
+++ app/fetch/get.js
@@ -2,8 +2,8 @@ import 'whatwg-fetch'
 import 'es6-promise'
 
 export function get(url) {
-  var host='https://test.huazilive.com/api/service';//测试地址
-  //var host='https://api.huazilive.com/api/service';//正式地址
+  // var host='https://test.huazilive.com/api/service';//测试地址
+  var host='https://api.huazilive.com/api/service';//正式地址
   var result = fetch(host+url, {
       credentials: 'include',
       headers: {
